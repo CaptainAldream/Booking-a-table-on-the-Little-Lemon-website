7d466aca9c9dcee89aa5da70303de294fb237a26
Aldrin John C. Derije <77729553+CaptainAldream@users.noreply.github.com>
2023-10-24 11:31:22 +0800

    Update ConfirmedBooking.js

---
+++ src/components/ConfirmedBooking.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ConfirmedBooking = () => {
+  return(
+    <div className='confirm'>
+    <div>
+    <h1>Booking has been <span>Confirmed!</span></h1>
+    </div>
+  );
+};
+export default ConfirmedBooking;
